c0cbcdbc429499d008d67df29f0696ee6c5d68dc
shitaljadhav123 <jshital465@gmail.com>
2023-08-03 14:18:07 +0530

    User service update and delete user test method done...

--- src/test/java/com/bikkadit/electronicstrore/service/UserServiceTest.java
+++ src/test/java/com/bikkadit/electronicstrore/service/UserServiceTest.java
@@ -6,22 +6,24 @@ import com.bikkadit.electronicstrore.repository.UserRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
+
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.junit.jupiter.MockitoExtension;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Optional;
 
 @SpringBootTest
 public class UserServiceTest {
 
-    @Mock
+    @MockBean
     private UserRepository userRepository;
 
-    @InjectMocks
+    @Autowired
     private UserService userService;
 
     @Autowired
@@ -31,8 +33,7 @@ public class UserServiceTest {
 
     @BeforeEach
     public void init(){
-
-        User.builder()
+        user = User.builder()
                 .name("Shital")
                 .email("shital@gmail.com")
                 .about("I am a software developer")
@@ -49,9 +50,10 @@ public class UserServiceTest {
 
         Mockito.when(userRepository.save(Mockito.any())).thenReturn(user);
 
+
         UserDto user1 = userService.createUser(mapper.map(user, UserDto.class));
 
-        System.out.println(user1);
+        System.out.println(user1.getName());
 
         Assertions.assertNotNull(user1);
 
@@ -59,4 +61,46 @@ public class UserServiceTest {
 
     }
 
+    //update
+    @Test
+    public void updateUserTest(){
+
+        String userId="";
+
+        UserDto userDto = UserDto.builder()
+                .name("shital Jadhav")
+                .about("This is updated user with details")
+                .gender("Female")
+                .imageName("abc.jpg")
+                .build();
+
+        Mockito.when(userRepository.findById(Mockito.anyString())).thenReturn(Optional.of(user));
+
+        Mockito.when(userRepository.save(Mockito.any())).thenReturn(user);
+
+        UserDto updatedUser = userService.updateUser(userDto, userId);
+
+       // UserDto updatedUser = mapper.map(user, UserDto.class);
+
+        System.out.println(updatedUser.getName());
+
+        Assertions.assertNotNull(userDto);
+
+        Assertions.assertEquals(userDto.getName(),updatedUser.getName(),"name is not validated");
+    }
+
+    //delete user test cases
+
+    @Test
+    public void deleteUserTest(){
+
+        String userId="userIdabc";
+
+        Mockito.when(userRepository.findById("userIdabc")).thenReturn(Optional.of(user));
+
+        userService.deleteUser(userId);
+
+        Mockito.verify(userRepository,Mockito.times(1)).delete(user);
+
+    }
 }
